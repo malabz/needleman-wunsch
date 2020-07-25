fa105c2999a113d64d69d6144e181486ee7d2413
heartunderblade <otoshimonogatari@163.com>
2020-07-25 21:45:34 +0800

    new features

---
+++ precache-manifest.3fe48ef4e160ce887cd113cf1605d8e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c11c4d992b53239523a5db14d5e99e99",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4a68e675fe6229db0f67",
+    "url": "./static/css/main.060e4fdc.chunk.css"
+  },
+  {
+    "revision": "bc3f36f9bf7ce92819ba",
+    "url": "./static/js/2.a65184ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a65184ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a68e675fe6229db0f67",
+    "url": "./static/js/main.5f7dcee3.chunk.js"
+  },
+  {
+    "revision": "27a989319cd464264d52",
+    "url": "./static/js/runtime-main.f7a30eca.js"
+  }
+]);
